5e738fd574263301d45fd06f621db39d7029adb1
noman shaber <nimrashaheen451@gmail.com>
2025-09-22 15:30:55 +0500

    protect

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("accessToken"); // 👈 localStorage से token check करो
-  return token ? children : <Navigate to="/login" />;
+  const token = localStorage.getItem("accessToken");
+  return token ? children : <Navigate to="/login" replace />;
 };
+
 
 export default ProtectedRoute;
